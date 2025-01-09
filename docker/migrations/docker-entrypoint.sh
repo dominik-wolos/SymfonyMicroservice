846e6ac167643ffa3e124f76cb20abe963e8cee3
dominik-wolos <dominik.wolos@gmail.com>
2025-01-09 16:17:33 +0100

    Remove return 1

--- docker/migrations/docker-entrypoint.sh
+++ docker/migrations/docker-entrypoint.sh
@@ -8,9 +8,7 @@ do
     attempt_left=$((attempt_left-1))
 
     if [ "${attempt_left}" -eq "0" ]; then
-
         (>&2 echo "MySQL did not answer. Aborting migrations.")
-        exit 1
     else
         (>&2 echo "Waiting for MySQL to be ready...")
     fi
